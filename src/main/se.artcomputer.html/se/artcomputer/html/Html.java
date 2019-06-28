ac5083c9e2de0126c24acc4e6a521c45c49a834d
Per Lundholm <per@artcomputer.se>
2019-06-28 09:04:41 +0200

    Sorted nodes alphabetically

--- src/main/se.artcomputer.html/se/artcomputer/html/Html.java
+++ src/main/se.artcomputer.html/se/artcomputer/html/Html.java
@@ -20,40 +20,20 @@ public class Html {
         // Utility class
     }
 
-    private static Node node(String nodeName, Attributes attributeList, Node... content) {
-        return new Node(nodeName, attributeList, new Nodes(content));
+    public static Node a() {
+        return node(A);
     }
 
-    private static Node node(String nodeName) {
-        return new Node(nodeName, new Attributes(), new Nodes());
+    public static Node a(Attribute attribute, Node... content) {
+        return node(A, attributes(attribute), content);
     }
 
-    private static Node node(String nodeName, Attribute... attributes) {
-        return new Node(nodeName, new Attributes(attributes), new Nodes());
+    public static Node a(Attributes attributes, Node... content) {
+        return node(A, attributes, content);
     }
 
     public static Attributes attributes(Attribute... attributes) {
         return new Attributes(attributes);
-    }
-
-    private static Node node(String nodeName, Node... contents) {
-        return new Node(nodeName, new Attributes(), new Nodes(contents));
-    }
-
-    public static Node html(Node... contents) {
-        return node(HTML, contents);
-    }
-
-    public static Node head(Node... contents) {
-        return node(HEAD, contents);
-    }
-
-    public static Node meta(Attributes attributes) {
-        return node(META, attributes);
-    }
-
-    public static Node link(Attributes attributes) {
-        return node(LINK, attributes);
     }
 
     public static Node body(Attributes attributes, Node... contents) {
@@ -62,18 +42,6 @@ public class Html {
 
     public static Node body(Node... contents) {
         return node(BODY, contents);
-    }
-
-    public static Node h1(Node... contents) {
-        return node(H1, contents);
-    }
-
-    public static Node h1(Attributes attributes, Node... contents) {
-        return node(H1, attributes, contents);
-    }
-
-    public static Node paragraph(Node... contents) {
-        return node(P, contents);
     }
 
     public static Node div() {
@@ -88,24 +56,40 @@ public class Html {
         return node(DIV, contents);
     }
 
-    public static Node a() {
-        return node(A);
-    }
-
-    public static Node a(Attribute attribute, Node... content) {
-        return node(A, attributes(attribute), content);
-    }
-
-    public static Node a(Attributes attributes, Node... content) {
-        return node(A, attributes, content);
-    }
-
     public static Node form(Attributes attributes, Node... content) {
         return node(FORM, attributes, content);
     }
 
+    public static Node h1(Node... contents) {
+        return node(H1, contents);
+    }
+
+    public static Node h1(Attributes attributes, Node... contents) {
+        return node(H1, attributes, contents);
+    }
+
+    public static Node head(Node... contents) {
+        return node(HEAD, contents);
+    }
+
+    public static Node html(Node... contents) {
+        return node(HTML, contents);
+    }
+
     public static Node input(Attribute... attributes) {
         return node(INPUT, attributes);
+    }
+
+    public static Node link(Attributes attributes) {
+        return node(LINK, attributes);
+    }
+
+    public static Node meta(Attributes attributes) {
+        return node(META, attributes);
+    }
+
+    public static Node paragraph(Node... contents) {
+        return node(P, contents);
     }
 
     public static Node submit(String text) {
@@ -118,5 +102,23 @@ public class Html {
 
     public static Node text(String textString) {
         return new Text(textString);
+    }
+
+    // Private parts
+
+    private static Node node(String nodeName, Node... contents) {
+        return new Node(nodeName, new Attributes(), new Nodes(contents));
+    }
+
+    private static Node node(String nodeName, Attributes attributeList, Node... content) {
+        return new Node(nodeName, attributeList, new Nodes(content));
+    }
+
+    private static Node node(String nodeName) {
+        return new Node(nodeName, new Attributes(), new Nodes());
+    }
+
+    private static Node node(String nodeName, Attribute... attributes) {
+        return new Node(nodeName, new Attributes(attributes), new Nodes());
     }
 }
